5b208376c70bb8dc2dc1a1bfe28962f7e72bd7ee
hrmoyes <hrmoyes@gmail.com>
2022-08-03 13:37:35 -0500

    set up routes for viewing freelancers, users, all of them or individual

--- project_3_api/server.js
+++ project_3_api/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
-
+const freelancerRoutes = require('./app/routes/freelancer_routes')
 // require middleware
 const errorHandler = require('./lib/error_handler')
 const replaceToken = require('./lib/replace_token')
@@ -67,6 +67,7 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(freelancerRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
