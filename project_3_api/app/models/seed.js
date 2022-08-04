348931b81bbf6f5724c4380d0ab5f7e0df7bdea8
Alys <alice.g.cooper@gmail.com>
2022-08-04 11:02:41 -0400

    Update seed data with new document fields

--- project_3_api/app/models/seed.js
+++ project_3_api/app/models/seed.js
@@ -5,10 +5,10 @@ const Service = require('./service')
 const db = require('../../config/db')
 
 const startServices = [
-    { name: 'Animal Psychic Reading', type: 'paranormal', rate: 60, owner: "62eaac5550f1685008953240"},
-    { name: 'Rhythmic Dance Lesson', type: 'dance', rate: 50, owner: "62eaac5550f1685008953240"},
-    { name: 'Custom Yodeling Song', type: 'music', rate: 200, owner: "62eaac5550f1685008953240"},
-    { name: 'Photoshop Services', type: 'design', rate: 5, owner: "62eaac5550f1685008953240"}
+    { name: 'Animal Psychic Reading', type: 'paranormal', description: "Let me communicate telepathically with your animal from anywhere in the world.", location: "Zoom Call", rate: 60, owner: "62eaac5550f1685008953240"},
+    { name: 'Rhythmic Dance Lesson', type: 'dance', description: "Learn how to unleash the animal within with our rhythmic dance lesson.", location: "Boston Common",  rate: 50, owner: "62eaac5550f1685008953240"},
+    { name: 'Custom Yodeling Song', type: 'music', description: "I will compose and perform a custom yodeling song set to the theme of your choice.", location: "Worldwide",  rate: 200, owner: "62eaac5550f1685008953240"},
+    { name: 'Photoshop Services', type: 'design', description: "I will make you hot virtually.", location: "Anywhere",  rate: 5, owner: "62eaac5550f1685008953240"}
 ]
 
 // first, we need to connect to the database
@@ -18,7 +18,7 @@ mongoose.connect(db, {
     .then(() => {
         // first we remove all of the services
         // here we can add something to make sure that we only delete services without an owner
-        Service.deleteMany( {owner: null} )
+        Service.deleteMany()
             .then(deletedServices => {
                 console.log('Here are the deleted services: \n', deletedServices);
                 // the next step is to use our startServices array to create our seeded services
