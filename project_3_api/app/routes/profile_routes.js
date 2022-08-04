aedf0076704a6b2470aee53a8909535f87aa3516
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-08-04 13:53:33 -0400

    Profile Routes fixed with secure auth for update and delete

--- project_3_api/app/routes/profile_routes.js
+++ project_3_api/app/routes/profile_routes.js
@@ -3,16 +3,13 @@ const passport = require('passport')
 
 // pull in Mongoose model for pets
 const User = require('../models/user')
-
 const customErrors = require('../../lib/custom_errors')
 const handle404 = customErrors.handle404
-const requireOwnership = customErrors.requireOwnership
 const removeBlanks = require('../../lib/remove_blank_fields')
 const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
 // ROUTES GO HERE
-// we only need three, and we want to set them up using the same conventions as our other routes, which means we might need to refer to those other files to make sure we're using our middleware correctly
 
 // POST -> create a profile
 // POST /profile/<user_id>
@@ -25,11 +22,8 @@ router.post('/profile/:userId', requireToken, removeBlanks, (req, res, next) => 
     User.findById(userId)
         .then(handle404)
         .then(user => {
-            // console.log('this is the user', user)
-            // console.log('this is prof', prof)
             // set the profile as the user's profile
             user.profile = prof
-            // console.log('this is the new user.profile', user.profile)
             // save the user
             return user.save()
         })
@@ -39,28 +33,20 @@ router.post('/profile/:userId', requireToken, removeBlanks, (req, res, next) => 
 })
 
 // UPDATE a Profile
-// PATCH /profiles/<user_id>/<profile_id>
-router.patch('/updateprofile/:userId/', requireToken, removeBlanks, (req, res, next) => {
-// router.patch('/profile/:userId/:profileId', requireToken, removeBlanks, (req, res, next) => {
+// PATCH /updateProfile
+router.patch('/updateProfile', requireToken, removeBlanks, (req, res, next) => {
     // get the user and the profile ids saved to variables
-    const userId = req.params.userId
+    const userId = req.user.id
     // const profileId = req.params.profileId
 
     // find our user
     User.findById(userId)
         .then(handle404)
         .then(user => {
-            // single out the profile (.id is a subdoc method to find something in an array of subdocs)
-            // console.log(user.profile._id)
-            // const theProfile = user.profile[0]._id
-            // console.log(theProfile)
             // make sure the user sending the request is the owner
-            // requireOwnership(req, user)
             user.profile = req.body.profile
             console.log('user.profile', user.profile)
-            // update the profile with a subdocument method
-            // theProfile.set(req.body.profile)
-            // return the saved pet
+            // return the saved profile
             return user.save()
         })
         .then(() => res.sendStatus(204))
@@ -68,7 +54,7 @@ router.patch('/updateprofile/:userId/', requireToken, removeBlanks, (req, res, n
 })
 
 // SHOW
-// GET /profile/5a7db6c74d55bc51bdf39793
+// GET /profile/<user_id>
 router.get('/profile/:userId', (req, res, next) => {
     const userId = req.params.userId;
     let profile
@@ -88,28 +74,25 @@ router.get('/profile/:userId', (req, res, next) => {
 })
 
 // // DELETE a Profile
-// // DELETE /profiles/<user_id>/<profile_id>
-router.delete('/profile/:userId/:profileId', requireToken, (req, res, next) => {
-// router.delete('/profile/:userId/:profileId', requireToken, (req, res, next) => {
+// // DELETE /profile
+router.delete('/profile', requireToken, (req, res, next) => {
+    // we are getting the profile of associated user and removing it, a user can only ever access their profile with this pattern (require ownership is redundant)
     // get the user and the profile ids saved to variables
-    const userId = req.params.userId
-    // const profileId = req.params.profileId
-    // then we find the pet
+    const userId = req.user.id
     User.findById(userId)
-        // handle a 404
+        // returns user object of user that made the request
         .then(handle404)
         // do stuff with the toy(in this case, delete it)
         .then(user => {
+            // we know that this is the user object from find by id
             console.log('here is the user: \n', user)
             console.log('here is the profile: \n', user.profile)
+
             // we can get the subdoc the same way as update
-            const theProfile = user.profile
-            // const theProfile = user.profile.id(profileId)
-            console.log('here is theProfile', theProfile)
-            // require that the user deleting this toy is the user's owner
-            // requireOwnership(req, user)
+             const theProfile = user.profile
             // call remove on the subdoc
             theProfile.remove()
+            console.log('user without profile?', user)
             // return the saved user
             return user.save()
         })
