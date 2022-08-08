c5d8b99d9c031721b32d7794b0cf91ce4b39a446
Alys <alice.g.cooper@gmail.com>
2022-08-08 16:34:57 -0400

    update seed user data

--- app/models/seed.js
+++ app/models/seed.js
@@ -6,19 +6,19 @@ const db = require('../../config/db')
 
 // update the owner ID after you've created the users
 const startServices = [
-    { name: 'Animal Psychic Reading', type: 'paranormal', description: "Let me communicate telepathically with your animal from anywhere in the world.", location: "Zoom Call", rate: 60, owner: "62f15d803cced3f772227a67"},
-    { name: 'Rhythmic Dance Lesson', type: 'dance', description: "Learn how to unleash the animal within with our rhythmic dance lesson.", location: "Boston Common",  rate: 50, owner: "62f15d803cced3f772227a68"},
-    { name: 'Custom Yodeling Song', type: 'music', description: "I will compose and perform a custom yodeling song set to the theme of your choice.", location: "Worldwide",  rate: 200, owner: "62f15d803cced3f772227a6f"},
-    { name: 'Photoshop Services', type: 'design', description: "I will make you hot virtually.", location: "Anywhere",  rate: 5, owner: "62f15d803cced3f772227a6f"},
+    { name: 'Animal Psychic Reading', type: 'paranormal', description: "Let me communicate telepathically with your animal from anywhere in the world.", location: "Zoom Call", rate: 60, owner: "62f17323a187b2219f5ff3d4"},
+    { name: 'Rhythmic Dance Lesson', type: 'dance', description: "Learn how to unleash the animal within with our rhythmic dance lesson.", location: "Boston Common",  rate: 50, owner: "62f17323a187b2219f5ff3d4"},
+    { name: 'Custom Yodeling Song', type: 'music', description: "I will compose and perform a custom yodeling song set to the theme of your choice.", location: "Worldwide",  rate: 200, owner: "62f17323a187b2219f5ff3d5"},
+    { name: 'Photoshop Services', type: 'design', description: "I will make you hot virtually.", location: "Anywhere",  rate: 5, owner: "62f17323a187b2219f5ff3d5"},
     // new seed information
-    { name: 'Anime Theme Song Creator', type: 'music', description: "I will compose a cong for your anime.", location: "remote", rate: 200, owner: "62f15d803cced3f772227a6b"},
-    { name: 'Custom Stuffed Animal', type: 'textiles', description: "Let me make a stuffed animal for you.", location: "remote",  rate: 50, owner: "62f15d803cced3f772227a6b"},
-    { name: 'Gerbil Reiki', type: 'spirituality', description: "I provide Reiki services to gerbils specifically through Zoom.", location: "Zoom",  rate: 80, owner: "62f15d803cced3f772227a6b"},
-    { name: 'Pavlovian Training', type: 'lifestyle', description: "Do you want to salivate on command? I can train you through Zoom!", location: "Anywhere",  rate: 25, owner: "62f15d803cced3f772227a6b"},
-    { name: 'Custom Short Story', type: 'literature', description: "Let me write a 5k word short story to your specifications.", location: "remote",  rate: 50, owner: "62f15d803cced3f772227a6b"},
-    { name: 'Unpaid Internship', type: 'professional', description: "Let me be your unpaid intern for a small fee!", location: "Anywhere",  rate: 1500, owner: "62f15d803cced3f772227a6b"},
-    { name: 'Yakity Sax Sound Effects On Command', type: 'music', description: "I will follow you around and play Yakity Sax when it would be funny.", location: "Travel Pending",  rate: 450, owner: "62f15d803cced3f772227a6a"},
-    { name: 'Macrame Wall Art', type: 'textiles', description: "I create unique macrame creations", location: "remote",  rate: 50, owner: "62f15d803cced3f772227a6a"},
+    { name: 'Anime Theme Song Creator', type: 'music', description: "I will compose a cong for your anime.", location: "remote", rate: 200, owner: "62f17323a187b2219f5ff3d6"},
+    { name: 'Custom Stuffed Animal', type: 'textiles', description: "Let me make a stuffed animal for you.", location: "remote",  rate: 50, owner: "62f17323a187b2219f5ff3d6"},
+    { name: 'Gerbil Reiki', type: 'spirituality', description: "I provide Reiki services to gerbils specifically through Zoom.", location: "Zoom",  rate: 80, owner: "62f17323a187b2219f5ff3d7"},
+    { name: 'Pavlovian Training', type: 'lifestyle', description: "Do you want to salivate on command? I can train you through Zoom!", location: "Anywhere",  rate: 25, owner: "62f17323a187b2219f5ff3d7"},
+    { name: 'Custom Short Story', type: 'literature', description: "Let me write a 5k word short story to your specifications.", location: "remote",  rate: 50, owner: "62f17323a187b2219f5ff3d8"},
+    { name: 'Unpaid Internship', type: 'professional', description: "Let me be your unpaid intern for a small fee!", location: "Anywhere",  rate: 1500, owner: "62f17323a187b2219f5ff3d8"},
+    { name: 'Yakity Sax Sound Effects On Command', type: 'music', description: "I will follow you around and play Yakity Sax when it would be funny.", location: "Travel Pending",  rate: 450, owner: "62f17323a187b2219f5ff3d8"},
+    { name: 'Macrame Wall Art', type: 'textiles', description: "I create unique macrame creations", location: "remote",  rate: 50, owner: "62f17323a187b2219f5ff3d8"},
 ]
 
 // first, we need to connect to the database
